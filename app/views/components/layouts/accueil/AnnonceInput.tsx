062a315cfcb7bf2f3a12c25a8d8903502f94e55e
TsitouhRanjafy <radohery09@gmail.com>
2025-07-17 16:40:15 +0300

    add profile preview

--- app/views/components/layouts/accueil/AnnonceInput.tsx
+++ app/views/components/layouts/accueil/AnnonceInput.tsx
@@ -1,9 +1,16 @@
-import { View, Image, TextInput } from "react-native";
+import { router } from "expo-router";
+import { View, Image, TextInput, TouchableOpacity } from "react-native";
 
 const SectionAnnonce = () => {
     return (
         <View className="w-full h-[70] flex flex-row justify-between items-center">
-            <Image source={require("../../../../assets/icons/user.png")} className="w-[45] h-[45]" />
+            <TouchableOpacity 
+                onPress={() => {
+                    router.push("../../../userProfile")
+                }}
+            >
+                <Image  source={require("../../../../assets/icons/user.png")} className="w-[45] h-[45]" />
+            </TouchableOpacity>
             <View className="w-[82%] h-[56] flex flex-row justify-between items-center pr-[12] border rounded-xl">
                 <TextInput
                     className="w-[90%] h-full pl-5 font-lato-regular text-xl"
