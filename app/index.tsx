331b2adb9f798af63cf959bc64be95183c4a1b75
Fanomezantsoa2003 <tsoutsougamer@gmail.com>
2025-09-21 19:48:05 +0300

    test commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import "./global.css";
 import { ActivityIndicator, View } from "react-native";
-import { useFonts } from 'expo-font';
-import { Stack, Tabs, Link } from 'expo-router';
+import { useFonts } from "expo-font";
+import { Stack, Tabs, Link } from "expo-router";
 
 import Welcome from "./welcome";
 import Chat from "./chat";
@@ -16,15 +16,14 @@ import OrderTracking from "./orderTracking";
 
 //import Panier from "./panier";
 
-
 export default function Index() {
   // Chargement de la police avant d'afficher l'ui
   const [fontsLoaded] = useFonts({
-    "Syne-Regular": require('./assets/fonts/Syne-Regular.ttf'),
-    "Syne-Bold": require('./assets/fonts/Syne-Bold.ttf'),
-    "Syne-SemiBold": require('./assets/fonts/Syne-SemiBold.ttf'),
-    "Lato-Bold": require('./assets/fonts/Lato-Bold.ttf'),
-    "Lato-Regular": require('./assets/fonts/Lato-Regular.ttf')
+    "Syne-Regular": require("./assets/fonts/Syne-Regular.ttf"),
+    "Syne-Bold": require("./assets/fonts/Syne-Bold.ttf"),
+    "Syne-SemiBold": require("./assets/fonts/Syne-SemiBold.ttf"),
+    "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
+    "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
   });
 
   if (!fontsLoaded) {
@@ -33,11 +32,9 @@ export default function Index() {
 
   return (
     <View>
+      <OrderTracking></OrderTracking>
 
-        <OrderTracking></OrderTracking>
-
-        {/* <Panier></Panier> */}
-
+      {/* <Panier></Panier> */}
     </View>
   );
 }
